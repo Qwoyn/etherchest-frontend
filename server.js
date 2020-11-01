21d92f28ad97f9a7d56dbe44bae0032a85d22488
Daniel Pittman <dpdanpittman@gmail.com>
2020-11-01 15:59:19 -0800

    redirect to https

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
-import sslRedirect from 'heroku-ssl-redirect';
+
 const express = require("express");
 const path = require("path");
 const app = express();
 
-// enable ssl redirect
-app.use(sslRedirect());
+//redirect to ssl
+if(process.env.NODE_ENV === 'production') {
+  app.use((req, res, next) => {
+    if (req.header('x-forwarded-proto') !== 'https')
+      res.redirect(`https://${req.header('host')}${req.url}`)
+    else
+      next()
+  })
+}
 
 app.use(express.static(path.join(__dirname, "build")));
 
