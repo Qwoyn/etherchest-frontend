d9cdc7d0f08e56c0ac79093c789be7a0bf1d9c2f
Daniel Pittman <dpdanpittman@gmail.com>
2020-11-01 14:25:18 -0800

    added ssl redirect

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
+import sslRedirect from 'heroku-ssl-redirect';
 const express = require("express");
 const path = require("path");
 const app = express();
+
+// enable ssl redirect
+app.use(sslRedirect());
 
 app.use(express.static(path.join(__dirname, "build")));
 
