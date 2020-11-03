1a9f406dd105bfd585cf47d8ee960cad47da4585
Daniel Pittman <dpdanpittman@gmail.com>
2020-11-03 14:33:57 -0800

    Update WelcomeCardMobile.js

--- src/components/WelcomeCardMobile.js
+++ src/components/WelcomeCardMobile.js
@@ -3,7 +3,6 @@ import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
-import { ParallaxBanner } from 'react-scroll-parallax';
 import Countdown from "react-countdown";
 
 const useStyles = makeStyles(theme => ({
