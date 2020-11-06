ebdc7f62b1552cca2fe7f2d0cfbdddfd895872ea
Daniel Pittman <dpdanpittman@gmail.com>
2020-11-05 19:13:12 -0800

    Update Inventory.js

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -875,7 +875,7 @@ export default function Inventory() {
           className={classes.background}
           centered
         >
-          <Tab label="Gems" icon={<CrystalIcon />} {...a11yProps(0)} className={classes.font}  />
+          <Tab label="Gems" icon={<CrystalIcon />} {...a11yProps(0)} className={classes.font} disabled/>
           <Tab label="Ducats" icon={<DucatIcon />} {...a11yProps(1)} className={classes.font} disabled />
           <Tab label="Guild" icon={<LandIcon />} {...a11yProps(2)} className={classes.font} disabled />
         </Tabs>
