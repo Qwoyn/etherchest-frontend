dcc8672431fb6393917298f147bd948c312d3cc4
Daniel Pittman <dpdanpittman@gmail.com>
2020-11-05 20:08:39 -0800

    Update TermsPage.js

--- src/components/TermsPage.js
+++ src/components/TermsPage.js
@@ -3,7 +3,7 @@ import {withRouter} from "react-router-dom";
 import { makeStyles } from '@material-ui/core/styles';
 import ExpansionPanel from '@material-ui/core/ExpansionPanel';
 import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
-import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
